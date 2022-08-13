a388efccf7ea7bbe66b97ec5103629ba61d8193c
Alexander <raskalovalexander@gmail.com>
2022-08-13 14:01:21 +0300

    feat: login page, normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { FC } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './pages';
 
 const App: FC = () => {
   return (
     <Routes>
-      <Route path='/' />
-      <Route path='/signin' />
-      <Route path='*' />
+      <Route path="/" />
+      <Route path="/signin" element={<Login />} />
+      <Route path="*" />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './Login.css';
+
+export const Login: FC = () => {
+  return(
+  <div className="login page__login">
+    <form className='form' action="">
+      <input type="text" />
+    </form>
+  </div>
+  );
+};
