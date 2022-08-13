851487a55dfd8674d829c541ffcbbe4e16017b90
Alexander <raskalovalexander@gmail.com>
2022-08-13 17:46:32 +0300

    feat: added global styles

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react';
-import { Form, Input } from '../../components';
+import { Button, Form, Input } from '../../components';
 import './Login.css';
 
 export const Login: FC = () => {
   return (
     <div className="login page__login">
       <Form title="Simple Hotel Check">
-        <Input title="Логин" id="email" name="email" type="email" />
-        <Input title="Пароль" id="password" name="password" type="password" />
+        <Input title="Логин" id="email" name="email" type="email" style='mb-24' />
+        <Input title="Пароль" id="password" name="password" type="password" style='mb-32' />
+        <Button>Войти</Button>
       </Form>
     </div>
   );
